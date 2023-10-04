ae6b92dc13ddb5ed5be48aa0d05c7ea734248ef1
bhursuprasannakumar <bhursuprasannakumar@gmail.com>
2023-10-05 00:12:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,26 +4,32 @@ import DesignAtalog from './components/DesignAtalog/DesignAtalog';
 import {
   Routes,
   Route,
- 
+  Link
+
 } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='Navbar'>
+      <h1 className='warning'>* For better experinece see this on dimensions 1503 x 1125 </h1>
+      <div>
+        <Link to="/" className="heading1"> Home Page</Link>
+        <Link to="/design" className="heading2" >Design Catalog</Link>
+      </div>
+
     </div>
+
   )
 }
-
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/design" element={<DesignAtalog/>} />
-    </Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/design" element={<DesignAtalog />} />
+      </Routes>
     </div>
   );
 }
